194ba524617e2c173e738d0bd47c779eced49852
William Bortoluzzi Pereira <williambortoluzzipereira@gmail.com>
2020-06-03 18:53:46 -0300

    Dia3

---
+++ web/src/routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Route, BrowserRouter} from 'react-router-dom';
+import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
+//cada rota é um componente
+const Routes = () =>{
+    return(
+        <BrowserRouter>
+            <Route component={Home} path="/" exact/>
+            <Route component={CreatePoint} path="/create-point"/>
+        </BrowserRouter>
+    );
+}
+export default Routes;
